6b77a140b1c2ad704dda6dabc26e48b98547a3a1
Jouni Huttunen <jouni.huttunen@solita.fi>
2013-01-15 13:03:43 +0200

    Serialize sitemap url objects to sitemap.xml file

---
+++ Solita.Sitemap/Model/SitemapUrlData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solita.Sitemap.Model
+{
+    public class SitemapUrlData
+    {
+        public string Url { get; set; }
+        public DateTime? LastModified { get; set; }
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public decimal? Priority { get; set; }
+    }
+
+    public enum ChangeFrequency
+    {
+        Always,
+        Hourly,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Never
+    }
+}
---
+++ Solita.Sitemap/Schema/SitemapModelConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solita.Sitemap.Model;
+
+namespace Solita.Sitemap.Schema
+{
+    public class SitemapModelConverter
+    {
+        public string Serialize(IEnumerable<SitemapUrlData> data)
+        {
+            var root = new urlset {url = data.Select(ToProductType).ToList()};
+            return root.Serialize();
+        }
+
+        private tUrl ToProductType(SitemapUrlData model)
+        {
+            var type = new tUrl { loc = model.Url };
+
+            if (model.LastModified.HasValue)
+            {
+                type.lastmodSpecified = true;
+                type.lastmod = model.LastModified.Value;
+            }
+
+            if (model.ChangeFrequency.HasValue)
+            {
+                type.changefreqSpecified = true;
+                type.changefreq = ToChangeFreqType(model.ChangeFrequency.Value);
+            }
+
+            if (model.Priority.HasValue)
+            {
+                type.prioritySpecified = true;
+                type.priority = model.Priority.Value;
+            }
+
+            return type;
+        }
+
+        private tChangeFreq ToChangeFreqType(ChangeFrequency model)
+        {
+            switch (model) 
+            {
+                case ChangeFrequency.Always:
+                    return tChangeFreq.always;
+                case ChangeFrequency.Hourly:
+                    return tChangeFreq.hourly;
+                case ChangeFrequency.Daily:
+                    return tChangeFreq.daily;
+                case ChangeFrequency.Weekly:
+                    return tChangeFreq.weekly;
+                case ChangeFrequency.Monthly:
+                    return tChangeFreq.monthly;
+                case ChangeFrequency.Yearly:
+                    return tChangeFreq.yearly;
+                case ChangeFrequency.Never:
+                    return tChangeFreq.never;   
+                default:
+                    return tChangeFreq.always;
+            }
+        }
+    }
+}
