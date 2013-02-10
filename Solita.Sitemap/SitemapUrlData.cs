9911dc0de76c190858de31b9403844d999758e9b
Jouni Huttunen <jouni.huttunen@solita.fi>
2013-02-10 02:25:31 +0200

    Implement Equals-method in SitemapUrlData

--- Solita.Sitemap/SitemapUrlData.cs
+++ Solita.Sitemap/SitemapUrlData.cs
@@ -8,6 +8,27 @@ namespace Solita.Sitemap
         public DateTime? LastModified { get; set; }
         public ChangeFrequency? ChangeFrequency { get; set; }
         public decimal? Priority { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SitemapUrlData) obj);
+        }
+
+        protected bool Equals(SitemapUrlData other)
+        {
+            return string.Equals(Url, other.Url) 
+                && LastModified.Equals(other.LastModified) 
+                && ChangeFrequency.Equals(other.ChangeFrequency) 
+                && Priority.Equals(other.Priority);
+        }
+
+        public override int GetHashCode()
+        {
+            return Url.GetHashCode();
+        }
     }
 
     public enum ChangeFrequency
